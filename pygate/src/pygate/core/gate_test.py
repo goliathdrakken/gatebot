0ca2d8b2e9264c6bdeeb15debf341fc2b3deba76
Jared Szechy <jared.szechy@gmail.com>
2011-03-06 17:46:15 -0500

    forgot new test file

---
+++ pygate/src/pygate/core/gate_test.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""Unittest for gatebot module"""
+
+import commands
+import datetime
+import time
+import logging
+import socket
+import unittest
+import gatebot
+
+from django.test import TestCase
+
+from pygate.core import defaults
+from pygate.core import kbevent
+from pygate.core import models
+from pygate.core import kb_common
+from pygate.core import units
+from pygate.core.net import gatenet
+
+
+LOGGER = logging.getLogger('unittest')
+
+class GatebotTestCase(TestCase):
+  def setUp(self):
+    del logging.root.handlers[:]
+    defaults.set_defaults()
+    #defaults.gentestdata()
+
+    models.Entry.objects.all().delete()
+
+    self.site, _ = models.GatebotSite.objects.get_or_create(name='default')
+
+    self.test_gate = models.Gate.objects.create(site=self.site,
+        name='Test Gate')
+
+    self.gatebot = gatebot.GatebotCoreApp()
+    self.env = self.gatebot._env
+    self.backend = self.env.GetBackend()
+
+    self.test_user = self.backend.CreateNewUser('tester')
+    self.test_token = self.backend.CreateAuthToken('core.onewire',
+        '0000111122223333', 'tester')
+
+    self.test_user_2 = self.backend.CreateNewUser('tester_2')
+    self.test_token_2 = self.backend.CreateAuthToken('rfid',
+        '1243136425', 'tester_2')
+    kb_common.AUTH_TOKEN_MAX_IDLE['core.onewire'] = 2
+    kb_common.AUTH_TOKEN_MAX_IDLE['rfid'] = 2
+
+    # Kill the gatebot latch processing thread so we can single step it.
+    self.service_thread = self.env._service_thread
+    self.env._threads.remove(self.service_thread)
+
+    self.gatebot._Setup()
+    self.gatebot._StartThreads()
+
+    self.client = gatenet.GatenetClient()
+    while True:
+      if self.client.Reconnect():
+        break
+
+  def tearDown(self):
+    for thr in self.env.GetThreads():
+      self.assert_(thr.isAlive(), "thread %s died unexpectedly" % thr.getName())
+    self.gatebot.Quit()
+    for thr in self.env.GetThreads():
+      self.assert_(not thr.isAlive(), "thread %s stuck" % thr.getName())
+    del self.gatebot
+    del self.env
+
+  def testSimpleFlow(self):
+    gate_name = self.test_gate.name
+    self.client.SendLatchStart(gate_name)
+
+    self.client.SendLatchStop(gate_name)
+    self.service_thread._FlushEvents()
+
+    drinks = self.test_keg.entries.valid()
+    self.assertEquals(len(drinks), 1)
+    last_entry = entries[0]
+
+    LOGGER.info('last entry: %s' % (last_entry,))
+
+    self.assertEquals(last_entry.user, None)
+
